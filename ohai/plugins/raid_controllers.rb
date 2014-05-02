5c1cb080643dda583bda80e5bbd3cb42178b5a00
Tim Smith <tsmith@llnw.com>
2014-05-02 11:47:27 -0700

    Rubocop fixes to the raid_controllers plugins

--- ohai/plugins/raid_controllers.rb
+++ ohai/plugins/raid_controllers.rb
@@ -1,31 +1,31 @@
-provides "raid/controllers"
+provides 'raid/controllers'
 
-if File.exists?("/usr/bin/lspci")
-   ls_pci_output = %x[/usr/bin/lspci -m | /bin/grep -i raid]
+if File.exist?('/usr/bin/lspci')
+  ls_pci_output = %x(/usr/bin/lspci -m | /bin/grep -i raid)
 else
-   return 0
+  return 0
 end
 
 if ls_pci_output.size < 1
-   return 0
+  return 0
 end
 
-raid_controllers Mash.new()
+raid_controllers Mash.new
 
 i = 0
 ls_pci_output.each_line do
    |line|
-   controller = line.split
-   raid_controllers[i.to_s] = Mash.new()
-   rs = String.new()
-   controller.slice(4..-1).each {
-     |e|
-     if rs == ''
-        rs = e.sub(/"/, '').chomp()
-     else
-        rs = rs + " " + e.sub(/"/,'').chomp()
-     end
-   }
-   raid_controllers[i.to_s][:type] = rs
-   i = i+1
+  controller = line.split
+  raid_controllers[i.to_s] = Mash.new
+  rs = String.new()
+  controller.slice(4..-1).each {
+    |e|
+    if rs == ''
+      rs = e.sub(/"/, '').chomp
+    else
+      rs = rs + ' ' + e.sub(/"/, '').chomp
+    end
+  }
+  raid_controllers[i.to_s][:type] = rs
+  i = i + 1
 end
